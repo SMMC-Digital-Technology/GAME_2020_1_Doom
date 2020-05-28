8d07ef89f789823661c7cd9142211483a6b3e575
Mason44 <TREVASMR@mackillop.act.edu.au>
2020-05-29 09:25:22 +1000

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -5,5 +5,21 @@
 var menuState = {
    create: function() {
       // create the scene
+  game.add.image(0, 0, "sky");
+  button = game.add.button(game.world.centerX, game.world.centerY, 'button');
+  button.anchor.setTo(0.5, 0.5);
+  button.onInputUp.add(this.startGame);
+  var text = game.add.text(button.x, button.y, 'Start Game');
+  text.anchor.setTo(0.5, 0.5);
+},
+
+
+startGame: function() {
+ game.global.score = 0;
+ game.global.lives = game.global.maxLives;
+ game.state.start("level1");
+
+}
+}
    }
 };
